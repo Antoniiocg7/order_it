2c1346307f1bb44228a52ee02851a3c951b09ca9
Antonio Cañizares Gamarra <antoniocanizares8820@gmail.com>
2024-05-24 12:47:28 +0200

    Overflow FirstPage solucionado

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -21,119 +21,108 @@ class FirstPage extends StatelessWidget {
         backgroundColor: Colors.green,
         //todo: action: logout
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              const SizedBox(
-                height: 20,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              '¡Usted elije como ser atendido!',
+              style: TextStyle(
+                fontSize: 20,
+                color: Theme.of(context).colorScheme.inversePrimary,
               ),
-              Text(
-                '¡Usted elije como ser atendido!',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 20),
+            Column(
+              children: [
+                // Contenedor llamar camarero
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CallWaiter(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade200,
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: Colors.green,
+                        width: 1,
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Lottie.network(
+                          height: 200,
+                          width: 300,
+                          "https://lottie.host/41971476-4e70-415a-84be-f27724801f80/Pr23iBZ13J.json",
+                        ),
+                        const Text(
+                          'Llamar a un camarero',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 15),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // Contenedor llamar camarero
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const CallWaiter(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.green.shade200,
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(
-                          color: Colors.green,
-                          width: 1,
-                        ),
+                const SizedBox(height: 25),
+                // Contenedor Pedir Online
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomePage(),
                       ),
-                      child: Column(
-                        children: [
-                          Lottie.network(
-                            height: 275,
-                            width: 275,
-                            "https://lottie.host/41971476-4e70-415a-84be-f27724801f80/Pr23iBZ13J.json",
-                          ),
-                          const Text(
-                            'Llamar a un camarero',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.black54,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          )
-                        ],
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade200,
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: Colors.green,
+                        width: 1,
                       ),
                     ),
-                  ),
-
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  // Contenedor Pedir Online
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomePage(),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Lottie.network(
+                          height: 200,
+                          width: 300,
+                          "https://lottie.host/21421179-7764-42b8-8c26-afe8e5c115a9/mr4rrQa9mX.json",
                         ),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.green.shade200,
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(
-                          color: Colors.green,
-                          width: 1,
+                        const Text(
+                          'Pedir Online',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                      ),
-                      child: Column(
-                        children: [
-                          Lottie.network(
-                            height: 275,
-                            width: 275,
-                            "https://lottie.host/21421179-7764-42b8-8c26-afe8e5c115a9/mr4rrQa9mX.json",
-                          ),
-                          const Text(
-                            'Pedir Online',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.black54,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          )
-                        ],
-                      ),
+                        const SizedBox(height: 15),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
